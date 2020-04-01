9f930b5f13022c6079567fc218dc3651690a47f8
wangfenjin <wangfenj@gmail.com>
2020-04-01 01:35:39 +0000

    update days
    Change-Id: I663d9b996605983b48882b8b0f99ae92a16710e1

--- presto-queries/query_21.sql
+++ presto-queries/query_21.sql
@@ -17,7 +17,7 @@ select  *
      and inv_warehouse_sk   = w_warehouse_sk
      and inv_date_sk    = d_date_sk
      and d_date between (cast ('1998-04-08' as date) - 30 days)
-                    and (cast ('1998-04-08' as date) + 30 days)
+                    and (cast ('1998-04-08' as date) + interval '30' day)
    group by w_warehouse_name, i_item_id) x
  where (case when inv_before > 0 
              then inv_after / inv_before 
